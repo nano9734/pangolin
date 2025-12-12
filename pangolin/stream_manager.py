f803221d8d9209fe84bbf906b21fa3374ddfa689
nano9734 <234749190+nano9734@users.noreply.github.com>
2025-12-12 18:12:38 +0900

    update stream_manager.py

--- pangolin/stream_manager.py
+++ pangolin/stream_manager.py
@@ -1,99 +1,48 @@
-import json
-import sqlite3
-import sys
-from contextlib import closing
-from datetime import datetime
-from urllib.parse import urlsplit, urlunsplit
-from zoneinfo import ZoneInfo
-from websocket import create_connection
-from .strategies import GetStrategy
+from urllib.parse import urlunsplit
 
 class StreamManager:
-    def __init__(self, config, database_file_name):
-        self.database_file_name = database_file_name
-        # create an instance of GetStrategy
-        self.get_strategy = GetStrategy()
+    def __init__(self, config, database):
+        print('*** StreamManager ***')
 
-        if config.stream_manager_is_enabled == 'yes':
-            # allowed exchange class names
-            class_names = []
-            for class_name in config.allowed_class_names:
-                if class_name not in ['config', 'logger', 'database', 'stream_manager']:
-                    class_names.append(class_name.capitalize())
+        for validated_exchange_name in config.validated_exchange_names:
+            wss_url_list = self._build_websocket_endpoint_list(
+                config=config,
+                supported_coin_list=self._build_supported_coin_list(config, validated_exchange_name),
+                validated_exchange_name=validated_exchange_name
+            )
 
-            # one class or multiple classes
-            if len(class_names) == 1:
-                class_name = class_names[0]
-                url = self.build_url(config, class_name=class_name, class_names=None)
-                self.process_stream_data(url)
-            else:
-                # url = self.build_url(config, class_name=None, class_names=class_names)
-                pass # multiple classes...
+    def _build_supported_coin_list(self, config, validated_exchange_name):
+        supported_coin_list_str = getattr(
+            config, f'{validated_exchange_name.lower()}_supported_coin_list'
+        )
 
-            #self.process_stream_data(url)
+        supported_coin_list = [
+            coin_name.strip() for coin_name in supported_coin_list_str.split(',')
+        ]
 
-    def build_url(self, config, class_name, class_names):
-        protocol = 'wss'
-        # construct Binance WebSocket stream URL
-        if class_name == 'Binance':
-            host = 'fstream.binance.com'
-            path = 'stream'
-            if class_names is None:
-                if config.binance_supported_coin_list == 'BTC':
-                    symbol = config.binance_supported_coin_list.lower() + 'usdt'
-                    params = f'streams={symbol}@aggTrade/{symbol}@markPrice'
-                    url_data = [protocol, host, path, params, '']
-                return urlunsplit(url_data)
+        if config.developer_mode_enabled == True:
+            print(f'[INFO] Successfully created {validated_exchange_name.lower()}_supported_coin_list: {supported_coin_list}')
 
-    def process_stream_data(self, url):
-        while True:
-            try:
-                if urlsplit(url).netloc == 'fstream.binance.com':
-                    table_name = 'binance_futures'
-                    json_data = self.retrieve_json_data(url)
-                    if json_data['stream'].endswith('aggTrade'):
-                        price = float(json_data['data']['p'])
-                        quantity = float(json_data['data']['q'])
-                        trade_time_ms = int(json_data['data']['T'])
-                        current_time_ms = int(datetime.now(ZoneInfo('Asia/Seoul')).timestamp() * 1000)
+        return supported_coin_list
 
-                        # connect to database
-                        conn = sqlite3.connect(self.database_file_name)
-                        cursor = conn.cursor()
+    def _build_websocket_endpoint_list(self, config, supported_coin_list, validated_exchange_name):
+        wss_url_list = []
+        for supported_coin_name in supported_coin_list:
+            wss_url = self._create_wss_url(supported_coin_name, validated_exchange_name)
+            if wss_url is not None:
+                wss_url_list.append(wss_url)
 
-                        # create an table if not exists
-                        cursor.execute(
-                            f"""
-                            CREATE TABLE IF NOT EXISTS {table_name} (
-                                id INTEGER PRIMARY KEY AUTOINCREMENT,
-                                price REAL,
-                                quantity REAL,
-                                trade_time INTEGER
-                            );
-                            """
-                        )
+        if config.developer_mode_enabled == True:
+            i = 1
+            for wss_url in wss_url_list:
+                print(f'[INFO] Successfully created wss_url_list[{i}]: {wss_url}')
+                i = i + 1
 
-                        # insert a value into table
-                        cursor.execute(
-                            f"""
-                            INSERT INTO {table_name} (price, quantity, trade_time)
-                            VALUES (:price, :quantity, :trade_time)
-                            """,
-                            {
-                                "price": price,
-                                "quantity": quantity,
-                                "trade_time": trade_time_ms
-                            }
-                        )
+        return wss_url_list
 
-                        conn.commit()
-                        conn.close()
-                        self.get_strategy.run()
+    def _create_wss_url(self, supported_coin_name, validated_exchange_name):
+        symbol = supported_coin_name.strip().lower() + 'usdt'
+        params = f'streams={symbol}@aggTrade/{symbol}@markPrice'
+        wss_url = urlunsplit(['wss', 'fstream.binance.com', 'stream', params, ''])
 
-            except KeyboardInterrupt:
-                print("\nProgram interrupted. Exiting...")
-                sys.exit()
-
-    def retrieve_json_data(self, url):
-        with closing(create_connection(url)) as conn:
-            return json.loads(conn.recv())
+        return wss_url
