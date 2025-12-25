115f7bdfd28757d728620d89bc36c505552b617b
nano9734 <234749190+nano9734@users.noreply.github.com>
2025-12-25 17:00:51 +0900

    update manager.py: good structure

--- pangolin/manager.py
+++ pangolin/manager.py
@@ -10,76 +10,207 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <https://www.gnu.org/licenses/>.
+"""
+this module uses strategy files to decide short/long trades.
+this module connects to an SQLite3 database.
+"""
 
-from contextlib import closing
-from websocket import create_connection
-from collections import deque
-from typing import Mapping
-from datetime import datetime
+# standard library to run manager
 import json
+import os
+import sqlite3
+
+# to get current time with timestamp
+from datetime import datetime
+
+# for better type hints...
+from typing import Mapping
+
+# short-lived WebSocket using 'with' statement
+from websocket import create_connection
+from contextlib import closing
+
+# load Pangolin local module(s)
+from .strategies import GetStrategy
 
 class StreamManager:
-    """ WebSocket-based trade stream processor
+    # database_file_path = pangolin/pangolin.db
+    DATABASE_FILE_NAME = 'pangolin.db'
 
-    """
-    def __init__(self, loaded_config: Mapping[str, str], wss_url: str):
-        self._config: Mapping[str, str] = loaded_config
-        self._wss_url: str = wss_url
+    def __init__(
+        self,
+        loaded_config: Mapping[str, str],
+        wss_url: str
+    ):
+        """ initializes the instance for connecting websocket stream.
 
-        # state
-        self._previous_trade_id: int | None = None
-        self._cumulative_price: float = 0.0
-        self._count: int = 0
+        Args:
+            loaded_config:
+            wss_url:
+        """
 
-        # previously processed price
-        self._last_price: float | None = None
+        # create an instance of GetStrategy()
+        self.get_strategy = GetStrategy()
+
+        # create an instance of Config()
+        self.loaded_config = loaded_config
+
+        # wss_url is created from UrlFactory()
+        self.wss_url = wss_url
+
+        # initialize last_values
+        self.last_trade_id = None
+        self.last_timestamp_sec = None
+        self.last_price = None
+
+        # initialize cumulative_values
+        self.cumulative_count: int = 0
+        self.cumulative_time: float = 0.0
+        self.cumulative_price: float = 0.0
+        self.cumulative_quantity: float = 0.0
 
     def run(self):
         with closing(
-            create_connection(self._wss_url)
+            create_connection(
+                self.wss_url
+            )
         ) as conn:
             try:
+                if os.path.exists(self.DATABASE_FILE_NAME):
+                    print(f'{self.DATABASE_FILE_NAME} exists')
+                    os.remove(self.DATABASE_FILE_NAME)
+
+                    if not os.path.exists(self.DATABASE_FILE_NAME):
+                        print(f'{self.DATABASE_FILE_NAME} has been deleted')
+
                 while True:
-                    # receive raw message from connection
+                    # receive message from connection
                     message = conn.recv()
 
-                    # parse received message into JSON object
+                    # convert string to JSON
                     json_data = json.loads(message)
 
-                    # convert data
+                    # prevent duplicate trades
                     trade_id = int(json_data['a'])
-                    symbol = str(json_data['s'])
-                    price = float(json_data['p'])
-                    timestamp = int(json_data['T']) / 1000 # convert milliseconds to seconds
 
-                    if self._last_price is None:
-                        self._last_price = price
+                    if trade_id == self.last_trade_id:
+                        continue # go to the next loop
 
-                    # add
-                    self._cumulative_price += price
+                    # store trade id as last trade id
+                    self.last_trade_id = trade_id
 
-                    # trade count
-                    self._count += 1
-                    if self._count == 15:
-                        # from float to int
-                        self._cumulative_price = float(self._cumulative_price)
+                    # convert data
+                    if json_data:
+                        symbol = str(json_data['s'])
+                        price = float(json_data['p'])
+                        quantity = float(json_data['q'])
+                        timestamp_ms = int(json_data["T"])
+
+                    # convert milliseconds to seconds
+                    if timestamp_ms:
+                        timestamp_sec = timestamp_ms / 1000
+
+                    # add last time
+                    if self.last_timestamp_sec is not None:
+                        time_diff = timestamp_sec - self.last_timestamp_sec
+                        self.cumulative_time += time_diff
+
+                    # add last price
+                    if self.last_price is None:
+                        self.last_price = price
+
+                    # add price
+                    self.cumulative_price += price
+
+                    # add quantity
+                    self.cumulative_quantity += quantity
+
+                    # save last timestamp
+                    self.last_timestamp_sec = timestamp_sec
+
+                    # add cumulative_count
+                    self.cumulative_count += 1
+
+                    # test_point (real-time): cumulative_time | cumulative_count | cumulative_price | cumulative_quantity
+                    print(f'{self.cumulative_time} | {self.cumulative_count} | {self.cumulative_price} | {self.cumulative_quantity}')
+
+                    if self.cumulative_time > 10:
+                        # create a database connection
+                        db_conn = self._create_db_conn()
+
+                        # create a cursor from the database connection
+                        cursor = db_conn.cursor()
+
+                        # create table if is needed
+                        cursor.execute(
+                            '''
+                            CREATE TABLE IF NOT EXISTS stocks (
+                                id INTEGER PRIMARY KEY AUTOINCREMENT,
+                                symbol TEXT,
+                                avg_price REAL,
+                                timestamp REAL
+                            )
+                            '''
+                        )
 
                         # count average price
-                        self._avg_price = self._cumulative_price / self._count
+                        self.avg_price = self.cumulative_price / self.cumulative_count
 
-                        # from float to int
-                        self._avg_price = float(self._avg_price)
-
-                        # get current time
+                        # get current timestamp
                         now = datetime.now()
-                        self.now = datetime.now().strftime('%Y-%m-%d %H:%M:%S')
+                        current_timestamp = now.timestamp()
+                        current_timestamp_str = now.strftime('%Y-%m-%d %H:%M:%S')
 
-                        # print
-                        print(f'[{self._count}] {self.now} | avg_price: {self._cumulative_price}/{self._count} -> {self._avg_price}')
+                        # print messeage
+                        print(f'[{self.cumulative_count}] {current_timestamp_str} | '
+                              f'avg_price: {self.cumulative_price}/{self.cumulative_count} -> {self.avg_price}')
 
-                        # reset
-                        self._count = 0
-                        self._cumulative_price = 0.0
+                        # insert a row of data (PEP 249 compliant)
+                        cursor.execute(
+                            '''
+                            INSERT INTO stocks (symbol, avg_price, timestamp)
+                            VALUES (?, ?, ?)
+                            ''',
+                            (symbol, self.avg_price, current_timestamp)
+                        )
+
+                        # commit the changes
+                        db_conn.commit()
+
+                        # fetch the last inserted row
+                        last_row_id = (cursor.lastrowid,)
+                        cursor.execute('SELECT * FROM stocks WHERE id = ?', last_row_id)
+                        last_row = cursor.fetchone()
+
+                        # print the last inserted row
+                        print(f'  ╰─> Last inserted row: {last_row}')
+
+                        # run strategy
+                        self.get_strategy.run(
+                            cursor=cursor # Sqlite3 cursor required
+                        )
+
+                        # close the database connection
+                        cursor.close()
+
+                        # reset the cumulative values for next loop
+                        self.cumulative_count = 0
+                        self.cumulative_time = 0.0
+                        self.cumulative_price = 0.0
+                        self.cumulative_quantity = 0.0
 
             except KeyboardInterrupt:
-                print("Interrupted!")
+                print("Interrupted by user")
+
+    def _create_db_conn(self) -> sqlite3.Connection:
+        """create a Connection object.
+
+        Returns:
+            sqlite3.Connection: A connection object to the SQLite database.
+
+        Raises:
+            No need to raise here, because the validation has already been processed.
+        """
+        return sqlite3.connect(
+            self.DATABASE_FILE_NAME
+        )
