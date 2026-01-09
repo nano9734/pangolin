3ccdc875bb1ec068f1607cd313883fd81e449851
nano9734 <234749190+nano9734@users.noreply.github.com>
2026-01-09 17:57:46 +0900

    Refactor: minor code improvements

--- pangolin/__main__.py
+++ pangolin/__main__.py
@@ -10,48 +10,65 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, see <https://www.gnu.org/licenses/>.
-""" Main entry point for running Pangolin
+"""Main Entry Point for Running Pangolin.
+This file (__main__.py) initializes components required before running the software,
+including configuration loading (Config), WebSocket URL generation (UrlFactory),
+and Manager (StreamManager) that handles real-time trade data and strategy module(s).
 
-This allows running the service with: python3 -m pangolin
+Execution flow:
+1. Load and validate the configuration file using Config.
+2. Generate the WebSocket URL for the specified trading pair using UrlFactory.
+3. Initialize StreamManager and start the infinite loop by calling `run()`.
+
+Additional Notes:
+- API calls are not handled in this module or in StreamManager.
+  They should be managed within the strategy module.
+- Terminate the program safely using Ctrl + C.
 """
 
+# Load Pangolin components
 from pangolin import Config
 from pangolin import UrlFactory
 from pangolin import StreamManager
 
+# Parse WebSocket URL into components (scheme, netloc, path, etc.)
 from urllib.parse import urlparse
 
 # define main function
 def main():
+    # Create a Config object
     config = Config()
+
+    # Load the configuration file
     loaded_config = config.load(
         file_path=config.CONFIG_FILE_NAME,
         allow_missing=False
     )
 
-    # url factory ?
+    # Create an instance of UrlFactory to generate WebSocket URL
     url_factory = UrlFactory()
 
+    # Check if Binance is enabled in the configuration
     if loaded_config['Binance'].getboolean('is_enabled'):
         base_url = loaded_config['Binance']['base_url']
         symbol = loaded_config['Binance']['supported_coin'].lower()
         parsed_url = urlparse(base_url)
 
-        wss_url = url_factory.create_wss_url(
-            netloc=parsed_url.netloc,
-            ticker=symbol + 'usdt'
-        )
+    # Generate the WebSocket URL
+    wss_url = url_factory.create_wss_url(
+        netloc=parsed_url.netloc,
+        ticker=symbol + 'usdt'
+    )
 
-        manager = StreamManager(
-            loaded_config=loaded_config['Binance'],
-            wss_url=wss_url
-        )
-    else:
-        manager = None
+    # Initialize the StreamManager
+    manager = StreamManager(
+        loaded_config=loaded_config['Binance'],
+        wss_url=wss_url
+    )
 
-    if manager is not None:
-        manager.run()
+    # Execute the manager to start the associated tasks
+    manager.run()
 
-# execute only run as a script
+# Execute only run as a script
 if __name__ == '__main__':
     main()
