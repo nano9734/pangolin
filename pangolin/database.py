658f8ffe11713b198741141d727ace516541e6e8
nano9734 <234749190+nano9734@users.noreply.github.com>
2026-01-13 18:47:59 +0900

    refactor: minor code update

--- pangolin/database.py
+++ pangolin/database.py
@@ -1,94 +1,69 @@
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 2 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful,
-# but WITHOUT ANY WARRANTY; without even the implied warranty of
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-# GNU General Public License for more details.
-#
-# You should have received a copy of the GNU General Public License
-# along with this program; if not, see <https://www.gnu.org/licenses/>.
+# SPDX-License-Identifier: GPL-2.0-or-later
 
 import os
 import sqlite3
+from datetime import datetime
+from dataclasses import dataclass
+
+@dataclass(frozen=True)
+class SqlFileNames:
+    CREATE_BINANCE_TABLE = "create_binance_table.sql"
+    INSERT_BINANCE_QUERY = "insert_binance_query.sql"
 
 class Database:
-    STOCKS_BINANCE_TABLE_SCHEMA = """
-    CREATE TABLE IF NOT EXISTS stocks (
-        id INTEGER PRIMARY KEY AUTOINCREMENT,
-        symbol TEXT,
-        avg_price REAL,
-        cumulative_quantity REAL,
-        current_timestamp TIMESTAMP
-    )
-    """
+    DATABASE_DELETED_MESSAGE = "[INFO] Database file ({}) has been deleted."
+    DATABASE_CONNECTION_SUCCESS_MESSAGE = "[INFO] Connected to database file ({})."
+    TABLE_CREATED_MESSAGE = '[INFO] Table "{}" has been created.'
+    COMMIT_SUCCESS_MESSAGE = "[INFO] Database changes have been committed."
+    CLOSE_CONNECTION_MESSAGE = "[INFO] Database connection has been closed."
 
-    STOCKS_BINANCE_INSERT_QUERY = """
-    INSERT INTO stocks (
-        symbol,
-        avg_price,
-        cumulative_quantity,
-        current_timestamp
-    ) VALUES (?, ?, ?, ?)
-    """
+    def __init__(self, sql_path:str, enabled_exchange_name:str, database_file_name: str, database_table_name: str):
+        self.sql_path = sql_path
+        self.enabled_exchange_name = enabled_exchange_name
+        self.database_file_name = database_file_name
+        self.database_table_name = database_table_name
+        self.create_binance_table = self.load_sql_file(self.sql_path + SqlFileNames.CREATE_BINANCE_TABLE)
+        self.insert_binance_query = self.load_sql_file(self.sql_path + SqlFileNames.INSERT_BINANCE_QUERY)
+        self.table_created_message = self.TABLE_CREATED_MESSAGE.format(self.database_table_name)
+        self.database_deleted_message = self.DATABASE_DELETED_MESSAGE.format(self.database_file_name)
+        self.database_connection_success_message = self.DATABASE_CONNECTION_SUCCESS_MESSAGE.format(self.database_file_name)
+        self.commit_success_message = self.COMMIT_SUCCESS_MESSAGE
+        self.close_connection_message = self.CLOSE_CONNECTION_MESSAGE
 
-    def __init__(self, enabled_exchange_name: str, database_file_name: str):
-        self.EXCHANGE_NAME = enabled_exchange_name
-        self.DATABASE_FILE_NAME = database_file_name
-        self.DATABASE_TABLE_NAME = 'stocks'
-        self.DATABASE_EXISTS_MSG             = f'[INFO] Database file ({self.DATABASE_FILE_NAME}) exists. It will be deleted by the algorithm.'
-        self.DATABASE_NOT_EXISTS_MSG         = f'[INFO] Database file ({self.DATABASE_FILE_NAME}) does not exist, nothing to delete.'
-        self.DATABASE_DELETED_MSg            = f'[INFO] Database file ({self.DATABASE_FILE_NAME}) has been deleted.'
-        self.DATABASE_CONNECTION_SUCCESS_MSG = f'[INFO] Connected to database file ({self.DATABASE_FILE_NAME})'
-        self.TABLE_CREATED_MSG               = f"[INFO] Table '{self.DATABASE_TABLE_NAME}' has been created."
-        self.STOCKS_ROWS_DELETED_MSG         = f'[INFO] All rows in the stocks table have been deleted.'
-        self.COMMIT_SUCCESS_MSG              = f'[INFO] Database changes have been committed.'
-        print('*** Database ***')
+    def connect(self) -> None:
+        self.conn = sqlite3.connect(self.database_file_name)
+        self.cursor = self.conn.cursor()
+        print(self.database_connection_success_message)
 
-        if self.database_exists():
-            print(self.DATABASE_EXISTS_MSG)
-            self.delete_file(verbose=True)
-            self.connect()
-            self.create_table()
-            print(self.TABLE_CREATED_MSG)
-            self.conn.commit()
-            print(self.COMMIT_SUCCESS_MSG)
-        else:
-            print(self.DATABASE_NOT_EXISTS_MSG)
-        print() # Add a line break for console readability
+    @property
+    def database_file_exists(self) -> bool:
+        return os.path.exists(self.database_file_name)
 
-    def database_exists(self) -> bool:
-        return os.path.exists(self.DATABASE_FILE_NAME)
-
-    def delete_file(self, verbose: bool = False) -> None:
-        if os.path.exists(self.DATABASE_FILE_NAME):
-            os.remove(self.DATABASE_FILE_NAME)
-            if verbose:
-                print(self.DATABASE_DELETED_MSG)
+    def delete_database_file(self) -> None:
+        os.remove(self.database_file_name)
+        print(self.database_deleted_message)
 
     def create_table(self) -> None:
-        if self.EXCHANGE_NAME == 'binance':
-            self.cursor.execute(self.STOCKS_BINANCE_TABLE_SCHEMA)
+        if self.enabled_exchange_name == "binance":
+            self.cursor.execute(self.create_binance_table)
+            print(self.table_created_message)
 
-    def insert_row(self, symbol: str, avg_price: float, cumulative_quantity: float, current_timestamp: float) -> None:
-        if self.EXCHANGE_NAME == 'binance':
-            required_values = (symbol, avg_price, cumulative_quantity, current_timestamp)
-            self.cursor.execute(self.STOCKS_BINANCE_INSERT_QUERY, required_values)
-            self.conn.commit()
-            print(self.COMMIT_SUCCESS_MSG)
-
-    def clear_table(self) -> None:
-        self.cursor.execute('DELETE FROM ' + self.DATABASE_TABLE_NAME)
-        print(self.STOCKS_ROWS_DELETED_MSG)
+    def save_changes(self) -> None:
         self.conn.commit()
-        print(self.COMMIT_SUCCESS_MSG)
+        print(self.commit_success_message)
 
-    def connect(self):
-        self.conn = sqlite3.connect(self.DATABASE_FILE_NAME)
-        self.cursor = self.conn.cursor()
-        print(self.DATABASE_CONNECTION_SUCCESS_MSG)
+    def insert_row(self, symbol: str, avg_price: float, cumulative_quantity: float, current_time: float) -> None:
+        required_values = (symbol, avg_price, cumulative_quantity, current_time)
+        self.cursor.execute(self.insert_binance_query, required_values)
 
-    def close(self):
+    def delete_all_stocks(self) -> None:
+        self.cursor.execute('DELETE FROM ' + self.database_table_name)
+        self.cursor.execute("DELETE FROM sqlite_sequence WHERE name=?", (self.database_table_name,))
+
+    def close(self) -> None:
         self.conn.close()
+        print(self.close_connection_message)
+
+    def load_sql_file(self, file_name: str) -> str:
+        with open(file_name, "r", encoding="utf-8") as sql_file:
+            return sql_file.read()
