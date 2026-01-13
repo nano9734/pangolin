b80863d5fdc9d17e260d224fb44c404db5ae94e6
nano9734 <234749190+nano9734@users.noreply.github.com>
2026-01-13 18:57:43 +0900

    refactor: update structure

--- pangolin/__main__.py
+++ pangolin/__main__.py
@@ -9,58 +9,110 @@ Notes:
 """
 
 import os
-from urllib.parse import urlparse
-
-import schedule
 import time
-
+import schedule
+from urllib.parse import urlparse
+from dataclasses import dataclass
 from pangolin import Config
+from pangolin import Client
+from pangolin import BinanceClient
+from pangolin import ClientFactory
 from pangolin import Database
 from pangolin import Manager
 
-PROJECT_NAME = "pangolin"
-PROJECT_NAME_CAP = PROJECT_NAME.capitalize()
-DATA_FOLDER_NAME = "data"
-SQL_FOLDER_NAME = "sql"
-ORDER_PLACE_FILE_NAME = "order_place.json"
-ORDER_PLACE_FILE_PATH = os.path.join(PROJECT_NAME, DATA_FOLDER_NAME, ORDER_PLACE_FILE_NAME)
-CONFIG_FILE_NAME = "pangolin.ini"
-DATABASE_FILE_NAME = "pangolin.db"
-DATABASE_TABLE_NAME = "stocks"
-BINANCE_EXCHANGE_NAME = "binance"
-BINANCE_EXCHANGE_NAME_CAP = BINANCE_EXCHANGE_NAME.capitalize()
-SEPARATOR = "***"
-CONFIG_SECTION_HEADER_NAME = f'{SEPARATOR} Config {SEPARATOR}'
-DATABASE_SECTION_HEADER_NAME = f'{SEPARATOR} Database {SEPARATOR}'
-MANAGER_SECTION_HEADER_NAME = f'{SEPARATOR} Manager {SEPARATOR}'
+@dataclass(frozen=True)
+class FileExtensions:
+    JSON = ".json"
+    INI = ".ini"
+    DATABASE = ".db"
+
+@dataclass(frozen=True)
+class Project:
+    NAME = "pangolin"
+    NAME_CAP = NAME.capitalize()
+
+@dataclass(frozen=True)
+class FileNames:
+    ORDER_PLACE = "order_place" + FileExtensions.JSON
+    CONFIG = Project.NAME + FileExtensions.INI
+    DATABASE = Project.NAME + FileExtensions.DATABASE
+
+@dataclass(frozen=True)
+class DirectoryNames:
+    DATA = "data"
+    STRATEGY = "strategies"
+    SQL = "sql"
+
+@dataclass(frozen=True)
+class ProjectPaths:
+    STRATEGY = os.path.join(Project.NAME, DirectoryNames.STRATEGY)
+    ORDER_PLACE = os.path.join(Project.NAME, DirectoryNames.DATA, FileNames.ORDER_PLACE)
+    SQL = os.path.join(Project.NAME, DirectoryNames.SQL) + os.sep
+
+@dataclass(frozen=True)
+class ExchangesNames:
+    BINANCE = "binance"
+    BINANCE_CAP = BINANCE.capitalize()
+
+@dataclass(frozen=True)
+class ClassNames:
+    CONFIG = "Config"
+    CLIENT = "Client"
+    DATABASE = "Database"
+    MANAGER = "Manager"
+
+@dataclass(frozen=True)
+class Messages:
+    LEFT_SEPARATOR = "*** "
+    RIGHT_SEPARATOR = " ***"
+    CONFIG_HEADER = LEFT_SEPARATOR + ClassNames.CONFIG + RIGHT_SEPARATOR
+    CLIENT_HEADER = LEFT_SEPARATOR + ClassNames.CLIENT + RIGHT_SEPARATOR
+    DATABASE_HEADER = LEFT_SEPARATOR + ClassNames.DATABASE + RIGHT_SEPARATOR
+    MANAGER_HEADER = LEFT_SEPARATOR + ClassNames.MANAGER + RIGHT_SEPARATOR
+
+@dataclass(frozen=True)
+class DatabaseTable:
+    NAME = "stocks"
+
+@dataclass(frozen=True)
+class Binance:
+    FUTURES_WSS_URL = "wss://fstream.binance.com"
 
 class UrlFactory:
-    CREATE_WSS_URL_SUCCESS_MSG = '[UrlFactory] WebSocket URL ({}) has been assembled.'
-
-    def create_binance_wss_url(self, enabled_exchange_name:str, netloc: str, ticker: str) -> str:
+    @staticmethod
+    def create_binance_wss_url(enabled_exchange_name:str, netloc: str, ticker: str) -> str:
         wss_url = 'wss://' + netloc + '/ws/' + ticker + '@aggTrade'
-        print(self.CREATE_WSS_URL_SUCCESS_MSG.format(wss_url))
+        print(f'[UrlFactory] WebSocket URL ({wss_url}) has been assembled.')
+        print() # add a line break for console readability
         return wss_url
 
 def main():
-    config = Config(config_file_name=CONFIG_FILE_NAME, config_section_header_name=CONFIG_SECTION_HEADER_NAME)
+    print(Messages.CONFIG_HEADER)
+    config = Config(config_file_name=FileNames.CONFIG)
     loaded_exchange_config = config.load(allow_missing=False)
+    if binance_enabled(loaded_exchange_config=loaded_exchange_config):
+        enabled_exchange_name = ExchangesNames.BINANCE
+        enabled_exchange_name_capitalized = ExchangesNames.BINANCE_CAP
+        binance_config = loaded_exchange_config[enabled_exchange_name_capitalized]
+        netloc = urlparse(Binance.FUTURES_WSS_URL).netloc
+        ticker = binance_config['supported_coin'].lower() + 'usdt'
+        wss_url = UrlFactory.create_binance_wss_url(enabled_exchange_name=enabled_exchange_name, netloc=netloc, ticker=ticker)
 
-    if is_binance_enabled(loaded_exchange_config=loaded_exchange_config):
-        enabled_exchange_name = BINANCE_EXCHANGE_NAME
-        enabled_exchange_name_capitalized = BINANCE_EXCHANGE_NAME_CAP
-        netloc = urlparse(loaded_exchange_config[enabled_exchange_name_capitalized]['wss_url']).netloc
-        ticker = loaded_exchange_config[enabled_exchange_name_capitalized]['supported_coin'].lower() + 'usdt'
-        wss_url = UrlFactory().create_binance_wss_url(enabled_exchange_name=enabled_exchange_name, netloc=netloc, ticker=ticker)
-        print() # add a line break for console readability
-
-    print(DATABASE_SECTION_HEADER_NAME)
-    database = Database(
-        project_name=PROJECT_NAME,
-        sql_folder_name=SQL_FOLDER_NAME,
+    print(Messages.CLIENT_HEADER)
+    client = ClientFactory.create_client(
         enabled_exchange_name=enabled_exchange_name,
-        database_file_name=DATABASE_FILE_NAME,
-        database_table_name=DATABASE_TABLE_NAME
+        config=binance_config
+    )
+    client.place_order()
+
+    print() # add a line break for console readability
+
+    print(Messages.DATABASE_HEADER)
+    database = Database(
+        sql_path = ProjectPaths.SQL,
+        enabled_exchange_name=enabled_exchange_name,
+        database_file_name=FileNames.DATABASE,
+        database_table_name=DatabaseTable.NAME
     )
 
     if database.database_file_exists:
@@ -70,13 +122,14 @@ def main():
     database.create_table()
     print() # add a line break for console readability
 
-    print(MANAGER_SECTION_HEADER_NAME)
+    print(Messages.MANAGER_HEADER)
     manager = Manager(
         enabled_exchange_name=enabled_exchange_name,
         loaded_exchange_config=loaded_exchange_config,
         wss_url=wss_url,
         database=database,
-        order_place_file_path=ORDER_PLACE_FILE_PATH
+        order_place_file_path=ProjectPaths.ORDER_PLACE,
+        strategy_folder_path=ProjectPaths.STRATEGY
     )
 
     if not order_place_file_exists():
@@ -94,9 +147,9 @@ def job():
     print("test job")
 
 def order_place_file_exists() -> bool:
-    return os.path.exists(ORDER_PLACE_FILE_PATH)
+    return os.path.exists(ProjectPaths.ORDER_PLACE)
 
-def is_binance_enabled(loaded_exchange_config):
+def binance_enabled(loaded_exchange_config):
     return loaded_exchange_config['Binance'].getboolean('is_enabled')
 
 if __name__ == '__main__':
